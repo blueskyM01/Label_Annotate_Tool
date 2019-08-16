d013201337dfee4f510f0510dab3e15c5ae74b07
Jianbing Yang <1226853967@qq.com>
2019-08-16 10:12:24 +0800

    版本1

--- run_this.py
+++ run_this.py
@@ -7,7 +7,7 @@ from PyQt5.QtCore import QRect, Qt, QTimer, pyqtSignal
 import cv2
 from Annotate_Console import *
 from collections import defaultdict
-import json
+import json, os
 
 class MyMainWinow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
@@ -16,6 +16,9 @@ class MyMainWinow(QMainWindow, Ui_MainWindow):
 
         self.OpendefaultPath = './'
         self.SavedefaultPath = './'
+        self.fpLength = int(self.m4_LengthP.text())
+        self.fpSize = int(self.m4_SizeP.text())
+        self.NumSize = float(self.m4_Num_Size.text())
         self.FeaturePointList = []
         self.m4_ShowAnnoWinWidth = self.m4_ShowImage.width()  # 打开标注图片窗口的宽度
         self.m4_ShowAnnoWinHeight = self.m4_ShowImage.height()  # 打开标注图片窗口的高度
@@ -24,8 +27,12 @@ class MyMainWinow(QMainWindow, Ui_MainWindow):
         self.m4_Back.clicked.connect(self.m4_RemoveFeaturePoint)
         self.m4_SetOpenPath.clicked.connect(self.m4_SetOpenDir)
         self.m4_SetSavePath.clicked.connect(self.m4_SetSaveDir)
+        self.m4_OpenCheckImage.clicked.connect(self.m4_ShowAnnoResult)
         self.m4_ShowImage.sendmsg.connect(self.m4_GetFeaturePoint)  # 自定义信号槽连接
-
+        self.m4_LengthP.textChanged.connect(self.LengthP_changed)
+        self.m4_SizeP.textChanged.connect(self.fpSize_changed)
+        self.m4_Num_Size.textChanged.connect(self.NumSize_changed)
+        self.m4_CloseCheckImage.clicked.connect(self.m4_CloseShowAnnoResult)
     # 打开标注图片
     def m4_OpenAnn(self):
         # Feature Point点集列表
@@ -54,15 +61,14 @@ class MyMainWinow(QMainWindow, Ui_MainWindow):
             dict_feature_point = defaultdict(list)
             dict_feature_point[m4_ImageName[-1]] = self.FeaturePointList
 
-
-            with open(self.SavedefaultPath + "/" + m4_ImageName[-1] + '.json', "w") as f:
+            with open(self.SavedefaultPath + m4_ImageName[-1] + '.json', "w") as f:
                 json.dump(dict_feature_point, f)
 
             # dumps 将数据转换成字符串
             # json_str = json.dumps(dict_feature_point)
             # loads: 将字符串转换为字典
             # new_dict = json.loads(json_str)
-            reply = QMessageBox.information(self, '提示', 'Label已保存到：' + self.SavedefaultPath + '/'+ m4_ImageName[-1] + '.json'
+            reply = QMessageBox.information(self, '提示', 'Label已保存到：' + self.SavedefaultPath + m4_ImageName[-1] + '.json'
                                             , QMessageBox.Ok, QMessageBox.Ok)
 
 
@@ -91,6 +97,17 @@ class MyMainWinow(QMainWindow, Ui_MainWindow):
         pixmap = QPixmap.fromImage(QImg)
         self.m4_ShowImage.setPixmap(pixmap)
 
+        # 标注图像显示
+    def m4_AnnImageResultShow(self, frame):
+        frame = frame.copy()
+        height, width, bytesPerComponent = frame.shape
+        bytesPerLine = 3 * width
+        cv2.cvtColor(frame, cv2.COLOR_BGR2RGB, frame)
+        QImg = QImage(frame.data, width, height, bytesPerLine, QImage.Format_RGB888)
+        pixmap = QPixmap.fromImage(QImg)
+        self.m4_CheckImage.setPixmap(pixmap)
+
+
     # 松开鼠标，获取特征点
     def m4_GetFeaturePoint(self, x0, y0):
         x_center, y_center = self.m4_CoordinateConvert(x0, y0, self.m4_ShowAnnoWinWidth,
@@ -100,7 +117,7 @@ class MyMainWinow(QMainWindow, Ui_MainWindow):
         self.FeaturePointList.append((x_center, y_center))
         frame_show = self.img.copy()
         # 画出特征点
-        self.m4_DrawFeaturePoint(self.FeaturePointList ,frame_show)
+        self.m4_DrawFeaturePoint(self.FeaturePointList ,frame_show, lineLength=self.fpLength, size=self.fpSize, Num_Size=self.NumSize)
         # 刷新标注特征点图像
         self.m4_AnnImageShow(frame_show)
         if len(self.FeaturePointList) != 0:
@@ -131,13 +148,13 @@ class MyMainWinow(QMainWindow, Ui_MainWindow):
         if len(self.FeaturePointList) != 0:
             self.FeaturePointList.pop()
             frame_show = self.img.copy()
-            self.m4_DrawFeaturePoint(self.FeaturePointList ,frame_show)
+            self.m4_DrawFeaturePoint(self.FeaturePointList ,frame_show, lineLength=self.fpLength, size=self.fpSize, Num_Size=self.NumSize)
             self.m4_AnnImageShow(frame_show)
         else:
             self.m4_Back.setEnabled(False)
 
     # 画出特征点
-    def m4_DrawFeaturePoint(self, FeaturePointList, frame_show, lineLength=20):
+    def m4_DrawFeaturePoint(self, FeaturePointList, frame_show, lineLength=20, size=8, Num_Size=2):
         count = 0
         for fPoint in FeaturePointList:
             count += 1
@@ -150,9 +167,9 @@ class MyMainWinow(QMainWindow, Ui_MainWindow):
             line2_y1 = fPoint[1] - lineLength
             line2_x2 = fPoint[0] - lineLength
             line2_y2 = fPoint[1] + lineLength
-            cv2.line(frame_show, (line1_x1, line1_y1), (line1_x2, line1_y2), (0, 255, 255), 8)
-            cv2.line(frame_show, (line2_x1, line2_y1), (line2_x2, line2_y2), (0, 255, 255), 8)
-            cv2.putText(frame_show, str(count), fPoint, cv2.FONT_HERSHEY_SIMPLEX, 2.5, (255, 255, 0), 8)
+            cv2.line(frame_show, (line1_x1, line1_y1), (line1_x2, line1_y2), (0, 255, 255), size)
+            cv2.line(frame_show, (line2_x1, line2_y1), (line2_x2, line2_y2), (0, 255, 255), size)
+            cv2.putText(frame_show, str(count), tuple(fPoint), cv2.FONT_HERSHEY_SIMPLEX, Num_Size, (255, 255, 0), size)
 
     def m4_SetOpenDir(self):
         self.OpendefaultPath = QFileDialog.getExistingDirectory(self, "请选择文件夹路径", "./")
@@ -161,8 +178,33 @@ class MyMainWinow(QMainWindow, Ui_MainWindow):
         self.SavedefaultPath = QFileDialog.getExistingDirectory(self, '设置Label保存目录！', './')
         self.m4_LabelDir.setText(self.SavedefaultPath)
 
+    def m4_ShowAnnoResult(self):
+        AnnImagePath, _ = QFileDialog.getOpenFileName(self, "请打开已标注的图片", self.OpendefaultPath)
+        imagename = AnnImagePath.split('/')[-1]
+        labename = self.SavedefaultPath + imagename + '.json'
+        if not os.path.exists(labename):
+            reply = QMessageBox.information(self, '提示', '请选择已标注的图片, 同时请正确设置Label保存的目录!', QMessageBox.Ok, QMessageBox.Ok)
+        else:
+            self.m4_CloseCheckImage.setEnabled(True)
+            with open(labename, 'r') as load_f:
+                load_dict = json.load(load_f)
+            fps = load_dict[imagename]
+            img = cv2.imread(AnnImagePath)
+            self.m4_DrawFeaturePoint(fps, img, lineLength=self.fpLength, size=self.fpSize, Num_Size=self.NumSize)
+            self.m4_AnnImageResultShow(img)
 
+    def m4_CloseShowAnnoResult(self):
+        self.m4_CheckImage.setPixmap(QtGui.QPixmap(":/pic/YY.jpg"))
+        self.m4_CloseCheckImage.setEnabled(False)
 
+    def LengthP_changed(self):
+        self.fpLength = int(self.m4_LengthP.text())
+
+    def fpSize_changed(self):
+        self.fpSize = int(self.m4_SizeP.text())
+
+    def NumSize_changed(self):
+        self.NumSize = float(self.m4_Num_Size.text())
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     myWin = MyMainWinow()
